572b62034490a83310512382e247a5d2277325db
MurodovShaxzod <shaxzodmurodov83@gmail.com>
2023-10-30 16:55:38 +0500

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,78 @@
-import './App.css';
-import PickedItems from './PickedItems/PickedItems';
-import YourCard from './YourCard/YourCard';
-import jsonFayl from './jsonFayl.json'
+import "./App.css";
+import PickedItems from "./PickedItems/PickedItems";
+import jsonFayl from "./jsonFayl.json";
+import { useState } from "react";
 
 function App() {
-  let data = jsonFayl.shoes
+  let data = jsonFayl.shoes;
+  const [info, setInfo] = useState(data);
+  const [newInfo, setNewInfo] = useState([]);
+  const [count, setCount] = useState(1);
+  const [btn, setBtn] = useState("ADD TO CARD");
+  const minusCount = () => {
+    setCount(count - 1);
+    if(count === 1) {
+      setBtn("ADD TO CARD")
+    }
+  }
+
+  const clickedItem = (itemId) => {
+    const updatedInfo = info.map((item) => {
+      if(item.id === itemId) {
+    setBtn(<i className='bx bx-check'></i>)
+        setNewInfo([
+          ...newInfo,
+          {
+            ...item,
+            purchased: true,
+          }
+        ]);
+        return {
+          ...item,
+          purchased: true,
+        };
+      }
+      return item
+    })
+    setInfo(updatedInfo)
+  }
   return (
     <div className="App">
-      <PickedItems data={data} />
-      <YourCard data={data} />
+      <PickedItems data={info} clickedItem={clickedItem} btn={btn}/>
+
+      <div className="container">
+        <span className="bg-span"></span>
+        <header>
+          <img
+            src="https://logos-download.com/wp-content/uploads/2016/03/Nike_logo_emblem_logotype.png"
+            alt="img"
+          />
+          <h1 className="header-title">Your card</h1>
+          <p>Your card is empty.</p>
+        </header>
+        <main>
+          <ul>
+            {
+              newInfo.map(item => (
+                <li key={item.id} className="your-card-item">
+                  <div className="your-card-img">
+                    <img src={item.picture} alt="img"/>
+                  </div>
+                  <div className="your-card-title">
+                    <h2 className="your-card-text">{item.name}</h2>
+                    <span>${item.price}</span>
+                    <div className="your-card-cound">
+                      <button onClick={minusCount}><i className='bx bx-chevron-left'></i></button>
+                      <p className="count">{count}</p>
+                      <button onClick={() => setCount(count + 1)}><i className='bx bx-chevron-right'></i></button>
+                    </div>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+        </main>
+      </div>
     </div>
   );
 }
--- src/PickedItems/PickedItems.js
+++ src/PickedItems/PickedItems.js
@@ -1,50 +1,46 @@
 import React from "react";
 
 function PickedItems(props) {
-  // constructor(props) {
-  //   super(props)
-  // }
-
-  // state = {
-  //   data: <YourCard />
-  // }
-
-  // stateFunc() {
-  //   this.setState(state => ({
-  //     data: <YourCard price="hello" />
-  //   }))
-  // }
-
-    let {data} = props
-    return(
-      <div className="container">
-        <span className="bg-span"></span>
-        <header>
-          <img src="https://logos-download.com/wp-content/uploads/2016/03/Nike_logo_emblem_logotype.png" alt="img"/>
-          <h1 className="header-title">Picked items</h1>
-        </header>
-        <main>
-          <ul>{data.map(item => (
-                  <li key={item.id}>
-                    <div className="card-picked">
-                      <div className="card-picked-img">
-                        <img src={item.picture} alt="img" />
-                      </div>
-                      <div className="card-picked-title">
-                        <h2>{item.name}</h2>
-                        <p>{item.information}</p>
-                      </div>
-                      <div className="card-picked-price">
-                        <span>${item.price}</span>
-                        <button id="add-to-card">ADD TO CARD</button>
-                      </div>
-                    </div>
-              </li>
-          ))}</ul>
-        </main>
-      </div>
-    )
+  let { data, clickedItem, btn } = props;
+  return (
+    <div className="container">
+      <span className="bg-span"></span>
+      <header>
+        <img
+          src="https://logos-download.com/wp-content/uploads/2016/03/Nike_logo_emblem_logotype.png"
+          alt="img"
+        />
+        <h1 className="header-title">Picked items</h1>
+      </header>
+      <main>
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              <div className="card-picked">
+                <div className="card-picked-img">
+                  <img src={item.picture} alt="img" />
+                </div>
+                <div className="card-picked-title">
+                  <h2>{item.name}</h2>
+                  <p>{item.information}</p>
+                </div>
+                <div className="card-picked-price">
+                  <span>${item.price}</span>
+                  <button
+                    id="add-to-card"
+                    onClick={() => clickedItem(item.id)}
+                    disabled={item.purchased ? true : false}
+                  >
+                    {btn}
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
-export default PickedItems
-
+export default PickedItems;
